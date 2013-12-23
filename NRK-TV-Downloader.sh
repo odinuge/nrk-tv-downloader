2920488947ddc11aa9eac28bec367c7ae3e32942
Odin Ugedal <odin@ugedal.com>
2013-12-23 23:24:15 +0100

    Switch to The MIT License (MIT)

--- NRK-TV-Downloader.sh
+++ NRK-TV-Downloader.sh
@@ -2,25 +2,30 @@
 #
 # NRK-TV-Dowloader
 #
-#             DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+# The MIT License (MIT)
 #
-#                    Version 2, December 2004
+# Copyright (c) 2013 Odin Ugedal <odin@ugedal.com>
 #
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 #
-# Copyright (C) 2013 Odin Ugedal <odinuge[at]gmail[dot]com>
-#
-# Everyone is permitted to copy and distribute verbatim or modified
-# copies of this license document, and changing it is allowed as long
-# as the name is changed.
-#
-#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-#
-#   0. You just DO WHAT THE FUCK YOU WANT TO.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 #
 # Contributors:
-# Henrik Lilleengen <mail[at]ithenrik[dot]com>
+# Henrik Lilleengen <mail@ithenrik.com>
 #
 
 VERSION="0.5.0"
