449f5a443b4933c4dce0b5db3889e51fa7199e5d
Odin Ugedal <odin@ugedal.com>
2016-01-09 22:54:45 +0100

    Update copyright year

--- nrk-tv-downloader.sh
+++ nrk-tv-downloader.sh
@@ -4,7 +4,7 @@
 #
 # The MIT License (MIT)
 #
-# Copyright (c) 2013 Odin Ugedal <odin@ugedal.com>
+# Copyright (c) 2015 Odin Ugedal <odin@ugedal.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
